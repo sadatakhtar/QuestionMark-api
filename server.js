f7ad98f9a9e52e63e2a87ca3b8ec3c265a8cdc85
Your Name <hiba.moh@yahoo.com>
2020-12-20 13:37:09 +0000

    signup, login changes

--- server.js
+++ server.js
@@ -119,48 +119,49 @@ app.get ('/selectedquestionpage/:id', async (req, res) => {
   }
 });
 
-  //SIGNUP
-  app.post ('/register', (req, res) => {
-    
-  const {username, email, password, confirm } = req.body;
+//SIGNUP
+app.post ('/register', (req, res) => {
+  const {username, email, password, confirm} = req.body;
 
   let errorArray = [];
 
-  !username || !email || !password || !confirm && errorArray.push({message: "Please enter all fields"});
-  password.length < 5 && errorArray.push({message: "Password should be at least 5 characters"});
-  password !== confirm && errorArray.push({message: "Passwords do not match"});
+  !username ||
+    !email ||
+    !password ||
+    (!confirm && errorArray.push ({message: 'Please enter all fields'}));
+  password.length < 5 &&
+    errorArray.push ({message: 'Password should be at least 5 characters'});
+  password !== confirm && errorArray.push ({message: 'Passwords do not match'});
 
-  if(errorArray.length > 0){
-    res.send({errorArray});
+  if (errorArray.length > 0) {
+    res.send ({errorArray});
+  } else {
+    // let hashedPassword = await bcrypt.hash(password, 10);
+    // console.log(hashedPassword);
 
-  }else{
-  
-  // let hashedPassword = await bcrypt.hash(password, 10);
-  // console.log(hashedPassword);
+    pool.query (
+      `insert into users (name, email, password) values ($1, $2, $3)`,
+      [username, email, password],
+      (error, result) => {
+        console.log (error, result);
 
-    pool.query(`insert into users (name, email, password) values ($1, $2, $3)`, 
-    [username, email, password], (error, result)=> {
-        console.log(error, result);
-
-        if(error){
-          res.status(400).send({error: "Database connection not established!"});
+        if (error) {
+          res
+            .status (400)
+            .send ({error: 'Database connection not established!'});
         }
 
-        if(result){
-          res.status(200).send({success: true, message: "Registration successfull. Please login"});
-        }else{
-          res.status(401).send({success: false});
+        if (result) {
+          res.status (200).send ({
+            success: true,
+            message: 'Registration successfull. Please login',
+          });
+        } else {
+          res.status (401).send ({success: false});
         }
-
-      })
-
-}
-  
-  
-
-
- 
-
+      }
+    );
+  }
 });
 
 //LOGIN
@@ -175,15 +176,17 @@ app.post ('/login', (req, res) => {
         res.status (400).send ({error: 'Database connection not established!'});
       }
 
-      if(result.rows.length > 0){
-         res.send({success: true, message: `Welcome ${username}` });
-      }else{
-         // res.status(401).send({message: "Wrong username/password combination"});
-          res.status(401).json({success: false, message: "Invalid username/password. Please register or try again"});
-
+      if (result.rows.length > 0) {
+        res.send ({success: true, message: `Welcome ${username}`});
+      } else {
+        // res.status(401).send({message: "Wrong username/password combination"});
+        res.status (401).json ({
+          success: false,
+          message: 'Invalid username/password. Please register or try again',
+        });
       }
     }
-  )
+  );
 });
 
 //SERVER LISTEN
