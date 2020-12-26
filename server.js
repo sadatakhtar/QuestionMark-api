667828534c04194116e0f88685a6a45c86d059bc
Your Name <hiba.moh@yahoo.com>
2020-12-26 01:59:21 +0000

    solving access denied

--- server.js
+++ server.js
@@ -42,7 +42,17 @@ if (process.env.NODE_ENV === 'production') {
 app.use (cors ());
 app.use (express.json ()); //allow use to  access request.body
 
+app.use (function (req, res, next) {
+  res.header ('Access-Control-Allow-Origin', '*');
+  res.header (
+    'Access-Control-Allow-Headers',
+    'Origin,X-Requested-With,Content-Type,Accept'
+  );
+  next ();
+});
+
 //ROUTES
+
 app.get ('/', (req, res) => {
   res.send ('Homepage here');
 });
