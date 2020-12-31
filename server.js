3508e02386379c1a24bc4cce2a60008e795c97d8
Your Name <hiba.moh@yahoo.com>
2020-12-31 18:52:46 +0000

    pulling the user id

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express ();
 path = require ('path');
 const cors = require ('cors');
 const Pool = require ('pg').Pool;
+const {query} = require ('express');
 require ('dotenv').config ();
 
 // we use process.env to contain our environment variables
@@ -334,7 +335,7 @@ app.post ('/register', (req, res) => {
 app.post ('/login', (req, res) => {
   const {username, password} = req.body;
 
-  pool.query (
+  const queryResult = pool.query (
     `select * from users where name=$1 and password=$2`,
     [username, password],
     (error, result) => {
@@ -343,7 +344,10 @@ app.post ('/login', (req, res) => {
       }
 
       if (result.rows.length > 0) {
-        res.send ({success: true, message: `Welcome ${username}`});
+        res.send ({
+          success: true,
+          message: `Welcome ${username} Userid: ${queryResult.rows[0].id}`,
+        });
       } else {
         // res.status(401).send({message: "Wrong username/password combination"});
         res.status (401).json ({
