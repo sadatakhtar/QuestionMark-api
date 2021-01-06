9f946834b62e805e09147f76df3a1de8a9f2661d
Your Name <hiba.moh@yahoo.com>
2021-01-06 23:50:52 +0000

    formated the date to only yy/mm/dd without time

--- server.js
+++ server.js
@@ -58,10 +58,12 @@ app.get ('/', (req, res) => {
   res.send ('Homepage here');
 });
 
+// all questions
+
 app.get ('/allquestions', async (req, res) => {
   try {
     const allquestions = await pool.query (
-      'select id, module_id,question_title, question,answers,question_date,views,rate from question'
+      `select id, module_id,question_title, question,answers,to_char(question_date,'DD-MM-YYYY'),views,rate from question`
     );
     const filter = await pool.query ('select id,module from module');
     const q_answers = await pool.query (
